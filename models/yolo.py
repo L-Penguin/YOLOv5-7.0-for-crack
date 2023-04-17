15d2d4e07e63a0fb339e89b75b1fdea961c39271
LxPenguinQAQ <L_Penguin@163.com>
2023-04-17 10:21:20 +0800

    update: 添加目标检测和实力分割解耦头

--- models/yolo.py
+++ models/yolo.py
@@ -89,6 +89,82 @@ class Detect(nn.Module):
         return grid, anchor_grid
 
 
+class Detect_Decoupled(nn.Module):
+    # YOLOv5 Detect head for detection models
+    stride = None  # strides computed during build
+    dynamic = False  # force grid reconstruction
+    export = False  # export mode
+
+    def __init__(self, nc=80, anchors=(), ch=(), inplace=True):  # detection layer
+        super().__init__()
+        self.nc = nc  # number of classes
+        self.no = nc + 5  # number of outputs per anchor
+        self.nl = len(anchors)  # number of detection layers
+        self.na = len(anchors[0]) // 2  # number of anchors
+        self.grid = [torch.empty(0) for _ in range(self.nl)]  # init grid
+        self.anchor_grid = [torch.empty(0) for _ in range(self.nl)]  # init anchor grid
+        self.register_buffer('anchors', torch.tensor(anchors).float().view(self.nl, -1, 2))  # shape(nl,na,2)
+        self.m_stem = nn.ModuleList(Conv(x, x, 1) for x in ch)  # stem conv
+        self.m_cls = nn.ModuleList(
+            nn.Sequential(Conv(x, x, 3), nn.Conv2d(x, self.na * self.nc, 1)) for x in ch)  # cls conv
+        self.m_reg_conf = nn.ModuleList(Conv(x, x, 3) for x in ch)  # reg_conf stem conv
+        self.m_reg = nn.ModuleList(nn.Conv2d(x, self.na * 4, 1) for x in ch)  # reg conv
+        self.m_conf = nn.ModuleList(nn.Conv2d(x, self.na * 1, 1) for x in ch)  # conf conv
+
+        self.inplace = inplace  # use inplace ops (e.g. slice assignment)
+        # 实例分割解耦
+        if isinstance(self, Segment_Decoupled):
+            self.seg = nn.ModuleList(nn.Sequential(Conv(x, x, 3), nn.Conv2d(x, self.na*32, 1)) for x in ch)
+
+    def forward(self, x):
+        z = []  # inference output
+        for i in range(self.nl):
+            x[i] = self.m_stem[i](x[i])  # conv
+
+            bs, _, ny, nx = x[i].shape
+            # 预测分类信息
+            x_cls = self.m_cls[i](x[i]).view(bs, self.na, self.nc, ny, nx).permute(0, 1, 3, 4, 2).contiguous()
+            #
+            x_reg_conf = self.m_reg_conf[i](x[i])
+            x_reg = self.m_reg[i](x_reg_conf).view(bs, self.na, 4, ny, nx).permute(0, 1, 3, 4, 2).contiguous()
+            x_conf = self.m_conf[i](x_reg_conf).view(bs, self.na, 1, ny, nx).permute(0, 1, 3, 4, 2).contiguous()
+
+            # 实例分割的解耦头
+            if isinstance(self, Segment_Decoupled):
+                x_seg = self.seg[i](x[i]).view(bs, self.na, 32, ny, nx).permute(0, 1, 3, 4, 2).contiguous()
+                x[i] = torch.cat([x_reg, x_conf, x_cls, x_seg], dim=4)
+            else:
+                x[i] = torch.cat([x_reg, x_conf, x_cls], dim=4)
+
+            if not self.training:  # inference
+                if self.dynamic or self.grid[i].shape[2:4] != x[i].shape[2:4]:
+                    self.grid[i], self.anchor_grid[i] = self._make_grid(nx, ny, i)
+
+                if isinstance(self, (Segment, Segment_Decoupled)):  # (boxes + masks)
+                    xy, wh, conf, mask = x[i].split((2, 2, self.nc + 1, self.no - self.nc - 5), 4)
+                    xy = (xy.sigmoid() * 2 + self.grid[i]) * self.stride[i]  # xy
+                    wh = (wh.sigmoid() * 2) ** 2 * self.anchor_grid[i]  # wh
+                    y = torch.cat((xy, wh, conf.sigmoid(), mask), 4)
+                else:  # Detect (boxes only)
+                    xy, wh, conf = x[i].sigmoid().split((2, 2, self.nc + 1), 4)
+                    xy = (xy * 2 + self.grid[i]) * self.stride[i]  # xy
+                    wh = (wh * 2) ** 2 * self.anchor_grid[i]  # wh
+                    y = torch.cat((xy, wh, conf), 4)
+                z.append(y.view(bs, self.na * nx * ny, self.no))
+
+        return x if self.training else (torch.cat(z, 1),) if self.export else (torch.cat(z, 1), x)
+
+    def _make_grid(self, nx=20, ny=20, i=0, torch_1_10=check_version(torch.__version__, '1.10.0')):
+        d = self.anchors[i].device
+        t = self.anchors[i].dtype
+        shape = 1, self.na, ny, nx, 2  # grid shape
+        y, x = torch.arange(ny, device=d, dtype=t), torch.arange(nx, device=d, dtype=t)
+        yv, xv = torch.meshgrid(y, x, indexing='ij') if torch_1_10 else torch.meshgrid(y, x)  # torch>=0.7 compatibility
+        grid = torch.stack((xv, yv), 2).expand(shape) - 0.5  # add grid offset, i.e. y = 2.0 * x - 0.5
+        anchor_grid = (self.anchors[i] * self.stride[i]).view((1, self.na, 1, 1, 2)).expand(shape)
+        return grid, anchor_grid
+
+
 class Segment(Detect):
     # YOLOv5 Segment head for segmentation models
     def __init__(self, nc=80, anchors=(), nm=32, npr=256, ch=(), inplace=True):
@@ -103,6 +179,23 @@ class Segment(Detect):
     def forward(self, x):
         p = self.proto(x[0])
         x = self.detect(self, x)
+        return (x, p) if self.training else (x[0], p) if self.export else (x[0], p, x[1])
+
+
+class Segment_Decoupled(Detect_Decoupled):
+    # YOLOv5 Segment head for segmentation models
+    def __init__(self, nc=80, anchors=(), nm=32, npr=256, ch=(), inplace=True):
+        super().__init__(nc, anchors, ch, inplace)
+        self.nm = nm  # number of masks
+        self.npr = npr  # number of protos
+        self.no = 5 + nc + self.nm  # number of outputs per anchor
+        self.m = nn.ModuleList(nn.Conv2d(x, self.no * self.na, 1) for x in ch)  # output conv
+        self.proto = Proto(ch[0], self.npr, self.nm)  # protos
+        self.detect_decoupled = Detect_Decoupled.forward
+
+    def forward(self, x):
+        p = self.proto(x[0])
+        x = self.detect_decoupled(self, x)
         return (x, p) if self.training else (x[0], p) if self.export else (x[0], p, x[1])
 
 
@@ -154,7 +247,7 @@ class BaseModel(nn.Module):
         # Apply to(), cpu(), cuda(), half() to model tensors that are not parameters or registered buffers
         self = super()._apply(fn)
         m = self.model[-1]  # Detect()
-        if isinstance(m, (Detect, Segment)):
+        if isinstance(m, (Detect, Detect_Decoupled, Segment, Segment_Decoupled)):
             m.stride = fn(m.stride)
             m.grid = list(map(fn, m.grid))
             if isinstance(m.anchor_grid, list):
@@ -188,10 +281,10 @@ class DetectionModel(BaseModel):
 
         # Build strides, anchors
         m = self.model[-1]  # Detect()
-        if isinstance(m, (Detect, Segment)):
+        if isinstance(m, (Detect, Detect_Decoupled, Segment, Segment_Decoupled)):
             s = 256  # 2x min stride
             m.inplace = self.inplace
-            forward = lambda x: self.forward(x)[0] if isinstance(m, Segment) else self.forward(x)
+            forward = lambda x: self.forward(x)[0] if isinstance(m, (Segment, Segment_Decoupled)) else self.forward(x)
             # m.stride = torch.tensor([s / x.shape[-2] for x in forward(torch.zeros(1, ch, s, s))])  # forward
             # 调整预训练batch-size为2，方便进行池化后归一化
             m.stride = torch.tensor([s / x.shape[-2] for x in forward(torch.zeros(2, ch, s, s))])  # forward
@@ -252,6 +345,7 @@ class DetectionModel(BaseModel):
         y[-1] = y[-1][:, i:]  # small
         return y
 
+    '''
     def _initialize_biases(self, cf=None):  # initialize biases into Detect(), cf is class frequency
         # https://arxiv.org/abs/1708.02002 section 3.3
         # cf = torch.bincount(torch.tensor(np.concatenate(dataset.labels, 0)[:, 0]).long(), minlength=nc) + 1.
@@ -261,6 +355,30 @@ class DetectionModel(BaseModel):
             b.data[:, 4] += math.log(8 / (640 / s) ** 2)  # obj (8 objects per 640 image)
             b.data[:, 5:5 + m.nc] += math.log(0.6 / (m.nc - 0.99999)) if cf is None else torch.log(cf / cf.sum())  # cls
             mi.bias = torch.nn.Parameter(b.view(-1), requires_grad=True)
+    '''
+
+    def _initialize_biases(self, cf=None):  # initialize biases into Detect(), cf is class frequency
+        # https://arxiv.org/abs/1708.02002 section 3.3
+        # cf = torch.bincount(torch.tensor(np.concatenate(dataset.labels, 0)[:, 0]).long(), minlength=nc) + 1.
+        m = self.model[-1]  # Detect() module
+
+        if m is Detect or m is Segment:
+            for mi, s in zip(m.m, m.stride):  # from
+                b = mi.bias.view(m.na, -1)  # conv.bias(255) to (3,85)
+                b.data[:, 4] += math.log(8 / (640 / s) ** 2)  # obj (8 objects per 640 image)
+                b.data[:, 5:5 + m.nc] += math.log(0.6 / (m.nc - 0.99999)) if cf is None else torch.log(
+                    cf / cf.sum())  # cls
+                mi.bias = torch.nn.Parameter(b.view(-1), requires_grad=True)
+        elif m in {Detect_Decoupled, Segment_Decoupled}:
+            for mi, s in zip(m.m_conf, m.stride):  # from
+                b = mi.bias.view(m.na, -1)  # conv.bias(255) to (3,85)
+                b.data += math.log(8 / (640 / s) ** 2)  # obj (8 objects per 640 image)
+                mi.bias = torch.nn.Parameter(b.view(-1), requires_grad=True)
+
+            for mi, s in zip(m.m_cls, m.stride):  # from
+                b = mi[-1].bias.view(m.na, -1)  # conv.bias(255) to (3,85)
+                b.data += math.log(0.6 / (m.nc - 0.99999)) if cf is None else torch.log(cf / cf.sum())  # cls
+                mi[-1].bias = torch.nn.Parameter(b.view(-1), requires_grad=True)
 
 
 Model = DetectionModel  # retain YOLOv5 'Model' class for backwards compatibility
@@ -324,7 +442,7 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
                 ASPPF, NewC3, SE, C3SE, C3_SE, CBAM, C3CBAM, C3_CBAM, CA, C3CA, C3_CA, ECA, C3ECA, C3_ECA, DCN, C3DCN,
                 C3DCN_CA, C3DCNCA_CA, NewC3CA, NewC3DCN, NewC3DCNCA, NewC3_CA, NewC3CA_CA, NewC3DCN_CA, NewC3DCNCA_CA,
                 NewC3_1CA, NewC3CA_1CA, NewC3DCN_1CA, NewC3DCNCA_1CA, NewC3_CA_1CA, NewC3CA_CA_1CA, NewC3DCN_CA_1CA,
-                NewC3DCNCA_CA_1CA, C3DCNCA, C3CA_CA, DCNv2, NewASPP
+                NewC3DCNCA_CA_1CA, C3DCNCA, C3CA_CA, DCNv2, NewASPP, NewASPPF
         }:
             # c1为输入通道数、c2为输出通道数
             c1, c2 = ch[f], args[0]
@@ -347,12 +465,12 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
             # 输出为通道之和
             c2 = sum(ch[x] for x in f)
         # TODO: channel, gw, gd
-        elif m in {Detect, Segment}:
+        elif m in {Detect, Detect_Decoupled, Segment, Segment_Decoupled}:
             # 添加输入通道的维度参数
             args.append([ch[x] for x in f])
             if isinstance(args[1], int):  # number of anchors
                 args[1] = [list(range(args[1] * 2))] * len(f)
-            if m is Segment:
+            if m in {Segment, Segment_Decoupled}:
                 # 保证参数为8的倍数，并乘以yaml配置文件中的gw网络宽度配置
                 args[3] = make_divisible(args[3] * gw, 8)
         elif m is Contract:
@@ -365,7 +483,6 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
             c2 = ch[f] // args[0] ** 2
         else:
             c2 = ch[f]
-
         m_ = nn.Sequential(*(m(*args) for _ in range(n))) if n > 1 else m(*args)  # module
         t = str(m)[8:-2].replace('__main__.', '')  # module type
         np = sum(x.numel() for x in m_.parameters())  # number params
